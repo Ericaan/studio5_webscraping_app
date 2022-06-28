18bf8956b15795524d3079fb34233d7b9da8df25
Flanciscop <61368740+Flanciscop@users.noreply.github.com>
2022-06-28 16:38:19 +1200

    final related html element
    we can now find all the same html element data after 2 are selected
    
    Co-Authored-By: Ericaan <64824204+Ericaan@users.noreply.github.com>

--- web_scraper.py
+++ web_scraper.py
@@ -1,40 +1,77 @@
+from tokenize import String
+from tracemalloc import stop
+from unittest import result
 from bs4 import BeautifulSoup
 import requests
 
+#1 is for the first element selected and 1a for the second element selected
 
-URL1 = 'https://www.geeksforgeeks.org/'
-URL1a = 'https://www.geeksforgeeks.org/'
+URL = 'https://www.geeksforgeeks.org/'
 
 # Page content from Website URL
-page1 = requests.get( URL1 )
-page1a = requests.get( URL1a )
+page = requests.get( URL )
+
 # parse html content
-soup1 = BeautifulSoup( page1.content , 'lxml')
-soup1a = BeautifulSoup( page1a.content , 'lxml')
+soup = BeautifulSoup( page.content , 'lxml')
+soup1 = BeautifulSoup( page.content , 'lxml')
+soup1a = BeautifulSoup( page.content , 'lxml')
 
 # define a pattern(certain text)
 pattern1 = 'How Learning To Code Can Change Your Life?'
 #define a second pattern of the same set of data you want
 pattern2 = 'Simplify Your Hiring Process with GFG “Get Hired” Job Portal'
 
-'''
-# Anchor tag
-text1 = soup.find_all('a', text = pattern)
-#print(text1)
-# Span tag
-text2 = soup.find_all('span', text = pattern) 
-#print(text2)
-# Heading tag
-text3 = soup.find_all('h1', text = pattern) 
-#print(text3)
-# List tag
-text4 = soup.find_all('li', text = pattern) 
-#print(text4)
-'''
 
-#find the html element and attributes of the pattern
-text1 = soup1.find('a', text = pattern1)
-text2 = soup1a.find('a', text = pattern2)
+#method to find what type of tag user input text is
+def find_tag_element(pattern):
+    # Anchor tag
+    a_tag = soup.find_all('a', text = pattern)
+    if a_tag is not None:
+        return "a"
+    # Span tag
+    span_tag = soup.find_all('span', text = pattern) 
+    if span_tag is not None:
+        return "span"
+    # Heading tag
+    h2_tag = soup.find_all('h2', text = pattern) 
+    if h2_tag is not None:
+        return "h2"
+    h3_tag = soup.find_all('h3', text = pattern) 
+    if h3_tag is not None:
+        return "h3"
+    h4_tag = soup.find_all('h4', text = pattern) 
+    if h4_tag is not None:
+        return "h1"
+    # List tag
+    li_tag = soup.find_all('li', text = pattern) 
+    if li_tag is not None:
+        return "li"
+    ul_tag = soup.find_all('ul', text = pattern) 
+    if ul_tag is not None:
+        return "ul"
+    ol_tag = soup.find_all('ol', text = pattern) 
+    if ol_tag is not None:
+        return "ol"
+    # div 
+    div_tag = soup.find_all('div', text = pattern)
+    if div_tag is not None:
+        return "div"
+    # p
+    p_tag = soup.find_all('p', text = pattern)
+    if p_tag is not None:
+        return "p"
+    
+#check if both inputs are the same tag
+element_tag1 = find_tag_element(pattern1)
+element_tag2 = find_tag_element(pattern2)
+if element_tag1 == element_tag2:
+    #find the html element and attributes of the pattern
+    element_tag = element_tag1
+    text1 = soup1.find(element_tag, text = pattern1)
+    text2 = soup1a.find(element_tag, text = pattern2)
+else:
+    print("Selected items not related.")
+
 
 #get all the parent elements of both patterns
 #define list for parent of first 
@@ -51,10 +88,38 @@ for parent in text2.parents:
     parents2.append(parent)
 
 
-#new list with all the common elements
-p1_as_set = set(parents1)
-intersection = p1_as_set.intersection(parents2)
-common_parents = list(intersection)
+#new list with all the common parent elements
+def intersection(p1, p2):
+    results = [x for x in p1 if x in p2]
+    return results
+common_parents = []
+common_parents = intersection(parents1,parents2)
 
-print(common_parents)
 
+#list of all the items with same html element tag
+related_items = soup.find_all(element_tag)
+# where our desired items will be collected
+final_items = []
+
+#method to determine if list contains all the parents in common parents
+def in_common_parents(common,my_list):
+    result = False
+    for x in common:
+        for y in my_list:
+            if x == y:
+                result = True
+        return result
+    return result
+
+#iterate through all the related items, get their parents, 
+# if all the parents from common parents are in the related items parents add them to final list
+for item in related_items:
+    temp = []
+    for parent in item.parents:
+        parent.clear()
+        temp.append(parent)
+    if in_common_parents(common_parents,temp):
+        final_items.append(item)
+    soup = BeautifulSoup( page.content , 'lxml')
+
+print(*final_items, sep="\n")
